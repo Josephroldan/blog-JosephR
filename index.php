27d2b120d9ebf6e472d4eb7e4e16ff6596c306b3
Joseph E. Roldan <omega6441@gmail.com>
2015-01-29 09:18:53 -0800

    have progress on database

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+      echo "1:LALALALALALA  2:1 what the hell are you doing, 1:dramatic gasp"
         ?>
     </body>
 </html>
