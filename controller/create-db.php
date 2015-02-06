3c0a6ee75ce16464bfcfcf57f4ffa2a3a52d30d4
Joseph E. Roldan <omega6441@gmail.com>
2015-02-06 09:19:36 -0800

    more progress but it is slow ,better results soon

--- controller/create-db.php
+++ controller/create-db.php
@@ -6,19 +6,22 @@ $connection = new mysqli($host, $username, $password);
 if ($connection->connect_error) {
     die("ERROR: " . $connection->connect_error);
 } else {
-   
+    
 }
 
 $exists = $connection->select_db($database);
- if(!$exists){
+if (!$exists) {
     $query = $connection->query("CREATE DATABASE $database");
-    if($query){
+    if ($query) {
         echo "database succesful noobs " . $database;
-    }  
- }
- else {
-     echo "data base already exists" ;  
     }
+} else {
+    echo "data base already exists";
+}
 
-     $query = $connection->query();
+$query = $connection->query("CREATE TABLE post ("
+. "id int(11) NOT NULL AUTO_INCREMENT ,"
+. "title varchar(255) NOT NULL,"
+. "post text NOT NULL,"
+. "PRIMARY KEY (id))");
 $connection->close();
