f76d31bf22ab7a1d9bde6506679176f9058724ec
Joseph E. Roldan <omega6441@gmail.com>
2015-02-09 10:44:10 -0800

    on video 22 catching up

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . "/../model/database.php");
 $connection = new mysqli($host, $username, $password);
 
 if ($connection->connect_error) {
-    die("ERROR: " . $connection->connect_error);
+    die("<p>ERROR: " . $connection->connect_error . "</p>");
 } else {
     
 }
@@ -13,15 +13,22 @@ $exists = $connection->select_db($database);
 if (!$exists) {
     $query = $connection->query("CREATE DATABASE $database");
     if ($query) {
-        echo "database succesful noobs " . $database;
+        echo "<p>database succesful noobs " . $database . "</p>";
     }
 } else {
-    echo "data base already exists";
+    echo "<p>data base already exists</p>";
 }
 
 $query = $connection->query("CREATE TABLE post ("
-. "id int(11) NOT NULL AUTO_INCREMENT ,"
-. "title varchar(255) NOT NULL,"
-. "post text NOT NULL,"
-. "PRIMARY KEY (id))");
+        . "id int(11) NOT NULL AUTO_INCREMENT ,"
+        . "title varchar(255) NOT NULL,"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+
+if ($query) {
+    echo "succesfully created table:posts";
+} else {
+    echo "<p>$connection->error</p>";
+}
 $connection->close();
