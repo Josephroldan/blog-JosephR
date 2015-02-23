a92750d0e268fc12a1612bea5f8055ea872a7d74
Joseph E. Roldan <omega6441@gmail.com>
2015-02-23 10:45:06 -0800

    on pace and on vid 44

--- controller/create-db.php
+++ controller/create-db.php
@@ -16,4 +16,14 @@ if ($query) {
 
 $query = $_SESSION["connection"]->query("CREATE TABLE users ("
         . "id int(11) NOT NULL AUTO_INCREMENT ,"
-        . "title varchar(255) NOT NULL,");
+        . "username varchar(30) NOT NULL,"
+        . "email varchar(50) NOT NULL,"
+        . "password char(128) NOT NULL,"
+        . "salt char(128) NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query){
+    echo "succesfully created table:users";
+} else {
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
